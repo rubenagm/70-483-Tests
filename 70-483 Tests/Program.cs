331656744b4034332a86e4168e2a321afd0eb110
Rubén Guardado <ruben.guardado@wp.mx>
2018-08-29 17:40:14 -0500

    Agregar archivos de proyecto.

---
+++ 70-483 Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _70_483_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
